ae378ecf0e55564808a0c5720b7b565c3bf9cde4
HeemahMuslad <rohimat.m1800276@st.futminna.edu.ng>
2025-01-03 23:38:10 +0100

    created the auth files

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import { Box } from "@chakra-ui/react";
-import type { ReactNode } from "react";
+import { Box } from '@chakra-ui/react';
+// import type { ReactNode } from "react";
 
-import Footer from "./Footer";
-import Header from "./Header";
+// import Footer from "./Footer";
+// import Header from "./Header";
 
-type LayoutProps = {
-  children: ReactNode;
-};
-
-const Layout = ({ children }: LayoutProps) => {
+// type LayoutProps = {
+//   children: ReactNode;
+// };
+// { children }: LayoutProps
+const Layout = () => {
   return (
     <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
-      <Box margin="8">
+      {/* <Box margin="8">
         <Header />
         <Box as="main" marginY={22}>
           {children}
         </Box>
         <Footer />
-      </Box>
+      </Box> */}
     </Box>
   );
 };
---
+++ src/lib/pages/auth/components/sideImage.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const sideImage = () => {
+  return (
+    <Box>
+      <image src="" alt="" />{' '}
+    </Box>
+  );
+};
+
+export default sideImage;
