c3226bace5ad118dacb3785df54fcac7eafbccc3
HeemahMuslad <rohimat.m1800276@st.futminna.edu.ng>
2025-01-11 23:52:13 +0100

    added the dashboard layout

---
+++ src/lib/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from './layout';
+const Dashboard = () => {
+  return (
+    <DashboardLayout>
+      <div>Dashboard</div>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
