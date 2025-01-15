dd82e4d1c6d091a322bd83a1bb7720e0d7732c16
HeemahMuslad <rohimat.m1800276@st.futminna.edu.ng>
2025-01-16 00:00:01 +0100

    cc

--- src/lib/pages/dashboard/index.tsx
+++ src/lib/pages/dashboard/index.tsx
@@ -1,12 +1,54 @@
+'use client';
 import React from 'react';
-import { Box, Text, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import DashboardLayout from '../../layout';
+import CustomIcon from './components/icon';
+import Selectnput from './components/SelectInput';
+
+import { MdDashboard } from 'react-icons/md';
+import ButtonElement from '~/lib/components/Button';
+import CardCarousel from "./components/Swiper"
+
 const Dashboard = () => {
   return (
     <DashboardLayout>
-      <VStack>
-        Dashboard
-        hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+      <VStack align={'start'}>
+        <Heading as="h3" mb="15px">
+          Find Your Best Real Estate
+        </Heading>
+        <VStack bg={'white'} w={'full'} h={'100vh'} rounded={'lg'} p={'20px'}>
+          <Flex
+            w="full"
+            mb={'10px'}
+            justifyContent={'space-between'}
+            align={'center'}
+          >
+            <Flex gap="5">
+              <CustomIcon as={MdDashboard} rounded="sm" />
+              <Selectnput placeholder="Rent a Building" fontWeight="semibold" />
+            </Flex>
+            <Flex gap="5">
+              <CustomIcon as={MdDashboard} rounded="sm" />
+              <Selectnput placeholder="Date" fontWeight="semibold" />
+            </Flex>
+            <Flex gap="5">
+              <CustomIcon as={MdDashboard} rounded="sm" />
+              <Selectnput placeholder="Location" fontWeight="semibold" />
+            </Flex>
+            <ButtonElement label=" Search" w={'15%'} />
+            <CustomIcon as={MdDashboard} rounded="sm" />
+          </Flex>
+          <Divider></Divider>
+          <CardCarousel/>
+        </VStack>
       </VStack>
     </DashboardLayout>
   );
